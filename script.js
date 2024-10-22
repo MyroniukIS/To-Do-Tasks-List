52988f7c6cd7f8b6be2503ef0db01cb628c69f2f
Myroniuk Ilona <knopkakyrs@gmail.com>
2024-10-22 16:10:31 +0100

    start files, markup, styles, functionality added

---
+++ script.js
@@ -0,0 +1,50 @@
+// constants declared for input button and task list area
+const taskInput = document.getElementById("task-input");
+const taskSection = document.getElementById("list");
+// const inputTask = document.getElementById("task-input").value.trim();
+
+// listener for the Enter key - Used to add a new task
+taskInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    createTask();
+  }
+});
+
+//the onclick event for the 'Add  button
+document.getElementById("push").onclick = function () {
+  createTask();
+};
+
+// the function that creates a task
+function createTask() {
+  if (taskInput.value.length == 0) {
+    alert("The task field is blank. Enter a task name and try again.");
+  } else {
+    // this block inserts HTML that creates each task into the task area div element
+    taskSection.innerHTML += `<div class="task"> <label id="taskname"> <input onclick="updateTask(this)" type="checkbox" id="check-task"> <p>${
+      document.getElementById("task-input").value
+    }</p> </label> <div class="delete"> <i class="uil uil-trash"></i></div></div >`;
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+    taskSection.offsetHeight >= 200
+      ? taskSection.classList.add("overflow")
+      : taskSection.classList.remove("overflow");
+  }
+  taskInput.value = "";
+}
+
+function updateTask(task) {
+  let taskItem = task.parentElement.lastElementChild;
+  if (task.checked) {
+    taskItem.classList.add("checked");
+  } else {
+    taskItem.classList.remove("checked");
+  }
+}
+
+// !!!!!!!!!!!!!!!!!Quiz
